f6378d678e3ecf030db136814cfe5038334f611d
Sri Harsha Chilakapati <hello@goharsha.com>
2018-02-21 16:44:55 +0530

    Added a tool to automatically fetch the updated binaries from master

---
+++ update-binaries.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+git checkout master
+npm run compile-ps
+npm run compile-js
+git checkout gh-pages
+git checkout master -- dist/
+rm index_bundle.js
+rm index_bundle.js.map
+rm index.html
+rm -rf resources
+mv dist/* .
+git commit -m "Updated new binaries"
+git push origin gh-pages
